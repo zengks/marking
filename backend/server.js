a42e63d6ca57f4c0fa49fc109288cc3011e65d6a
Chen Zeng <info@czeng.ca>
2023-02-27 17:20:27 -0800

    added routes and controllers for userRegister and userLogin

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome'
+    })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
